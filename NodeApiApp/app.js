19fbc9e5fb6436cfc0c2b5108bed5582a67f6e85
Pedro Castano <pedro.pdc.83@gmail.com>
2018-03-04 19:42:10 -0300

    db connection done

--- NodeApiApp/app.js
+++ NodeApiApp/app.js
@@ -4,10 +4,19 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+//Mongo data base connection
+var mongoDB = 'mongodb://admin:admin@ds153958.mlab.com:53958/pdc_library';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+//APP configuration
 var app = express();
 
 // view engine setup
